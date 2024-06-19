dd639a792f467c9216ae579d44d2e4124590e890
bladupsy <gisela.cristaldo11@gmail.com>
2024-06-18 23:54:42 -0300

    Login view

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
@@ -9,4 +18,13 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+    formLogin: FormGroup;
+    constructor(private fb: FormBuilder){
+      this.formLogin = this.fb.group(
+        {
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required]]
+        },
+      );
+    }
 }
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
